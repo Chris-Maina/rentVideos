d0c5eb20df5a2737f203c7cc18edb8bcf5077e33
Chris Maina <chris.maina@andela.com>
2018-06-17 01:34:58 +0300

    feat(rentvideos): create the delete operation

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ const findVideo = id => videos.find(video => video.id === parseInt(id));
  * @summary validates inout
  * @param {Object} data - name of the video supplied
  */
-const validateInput = (data) => {
+const validateInput = (data, res) => {
   const schema = {
     name: Joi.string().min(3).required(),
   }
 
   const { error } = Joi.validate(data, schema);
-  if (error) return res.status(400).send({ message: error.details[0].message});
+  if (error) return error.details[0].message;
 }
 
 app.get('/', (req, res) => {
@@ -57,7 +57,8 @@ app.post('/api/videos', (req, res) => {
    * IF error, 400(BAD REQUEST)
    * ELSE 200(OK)
    */
-  validateInput(req.body);
+  const errorMsg = validateInput(req.body);
+  if(errorMsg) return res.status(400).send({ message: errorMsg});
 
   const video = {
     id: videos.length +1,
@@ -80,11 +81,29 @@ app.put('/api/videos/:id', (req, res) => {
   let video = findVideo(req.params.id);
   if (!video) return res.status(404).send({ message: 'The video with the given Id does not exist'});
 
-  validateInput(req.body);
+  const errorMsg = validateInput(req.body);
+  if(errorMsg) return res.status(400).send({ message: errorMsg});
 
   video.name = req.body.name;
-  videos = [...videos, video];
   res.send({ data: video, message: `Successfully edited ${video.name}`});
+});
+
+app.delete('/api/videos/:id', (req, res) => {
+   /**
+   * Look up the video with id,
+   * IF not found 404
+   * 
+   * Delete
+   * 
+   * Return the deleted obj
+   */
+  let video = findVideo(req.params.id);
+  if (!video) return res.status(404).send({ message: 'The video with the given Id does not exist'});
+
+  const index = videos.indexOf(video);
+  videos.splice(index, 1);
+
+  res.send({ data: video, message: `Successfully deleted ${video.name}`});
 });
 
 const port = process.env.PORT;
