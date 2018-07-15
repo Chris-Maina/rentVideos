c306bbf6433a2bc6e559de96a0103faea362a705
Chris Maina <chris.maina@andela.com>
2018-07-15 10:11:19 +0300

    feat(verifytoken) add a function to verify if a token exists

--- app/routes.js
+++ app/routes.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 
 // controllers
 const userController = require('./controllers/userController');
-const videoController = require('./controllers/videoController'); 
+const videoController = require('./controllers/videoController');
+const verifyToken = require('./controllers/verifyToken');
 
 // routes
 router.get('/', videoController.index);
 router.post('/user/register', userController.userExists, userController.registerUser);
 router.post('/user/login', userController.loginUser);
-router.get('/videos', videoController.getVideos);
+router.get('/videos', verifyToken,videoController.getVideos);
 router.get('/videos/:slug', videoController.getVideo);
-router.post('/videos', videoController.createVideo);
+router.post('/videos', verifyToken,videoController.createVideo);
 router.patch('/videos/:slug', videoController.updateVideo);
 router.delete('/videos/:slug', videoController.deleteVideo);
 
