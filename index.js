9eabe4148eed7eebf7fceda517fc339a2a8ea258
Chris Maina <chris.maina@andela.com>
2018-08-22 07:04:37 +0300

    Implement CRUD operations on array of videos using GraphQL (#8)
    * feat(query): setup GraphQL and create queries and mutations
    
     - add queries to get a video, getVideo, and get all videos, getVideos
     - add a mutation to create a video
     - add a mutation to update a video
     - add a mutation to create video
     - add function to validate input/data used to create a video
     - add a mutation to handle deleting of a video

--- index.js
+++ index.js
@@ -1,110 +1,139 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').load();
-const Joi = require('joi');
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
 app.use(express.json());
 
+// graphql schema
+const schema = buildSchema(
+  `type Query {
+    video(id: String): Video
+    videos: [Video]
+  }
+  type Mutation {
+    updateVideo(id: String, name: String): Video
+    createVideo(name: String, description: String): Video
+    deleteVideo(id: String): Video
+  }
+  type Video {
+    id: Int
+    name: String
+    description: String
+  }
+  `
+);
+
 let videos = [
-  {id: 1, name: 'Spiderman'},
-  {id: 2, name: 'Infinity War'},
-  {id: 3, name: 'Jurasic Park'},
+  {id: 1, name: 'Spiderman', description: 'Awesome spiderman movie'},
+  {id: 2, name: 'Infinity War', description: 'War of the avengers'},
+  {id: 3, name: 'Jurasic Park', description: 'Animal movie'},
 ];
 
 /**
  * @name findVideo
- * @summary Finds the video requested
- * @param {String} id - id for requested video
+ * @summary gets the video with the supplied id
+ * @param {String} id 
+ * @returns {Object} video
  */
-const findVideo = id => videos.find(video => video.id === parseInt(id));
+const findVideo = (id) => (videos.find(video => video.id === parseInt(id)));
+
+/**
+ * @name getVideo
+ * @summary Gets the video requested
+ * @param {Object} args 
+ * @returns {Object}
+ */
+const getVideo = args => {
+  const { id } = args;
+  return findVideo(id);
+}
+
+/**
+ * @name getVideos
+ * @summary Gets all the videos
+ * @param {Array} videos
+ */
+const getVideos = () => {
+  return videos;
+}
+
+/**
+ * @summary Updates the video requested
+ * @param {Object} args
+ * @returns {Object}
+ */
+const updateVideo = (args) => {
+  const { id, name } = args;
+  const video = findVideo(id);
+  video.name = name;
+  return video;
+}
 
 /**
  * @name validateInput
  * @summary validates inout
  * @param {Object} data - name of the video supplied
  */
-const validateInput = (data, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-
-  const { error } = Joi.validate(data, schema);
-  if (error) return error.details[0].message;
+const validateInput = (data) => {
+  const bodySchema = Joi.object().keys({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+  });
+  return Joi.validate(data, bodySchema);
 }
 
-app.get('/', (req, res) => {
-  res.send('Welcome to RentVideos, get entertained!');
-});
-
-app.get('/api/videos', (req, res) => {
-  res.send({ data: videos, message: 'Successfully fetched all the videos'});
-});
-
-app.get('/api/videos/:id', (req, res) => {
-  /**
-   * Find the video
-   * IF it does not exist, 404(NOT found)
-   * ELSE 200, with video
-   */
-  const video = findVideo(req.params.id);
-  if (!video) return res.status(404).send({ message: 'The video with the given Id does not exist'});
-  res.send({ data: video, message: 'Successfully fetched video requested'});
-});
-
-app.post('/api/videos', (req, res) => {
-  /**
-   * Validate the input
-   * IF error, 400(BAD REQUEST)
-   * ELSE 200(OK)
-   */
-  const errorMsg = validateInput(req.body);
-  if(errorMsg) return res.status(400).send({ message: errorMsg});
-
-  const video = {
-    id: videos.length +1,
-    name: req.body.name
+/**
+ * @summary Adds the created video to list of videos
+ * @param {object} args
+ * @returns {Object} newVideo 
+ */
+const createVideo = (args) => {
+  const { error, value } = validateInput(args);
+  if (error ) return error;
+  if (value) {
+    const newVideo = {
+      id: videos.length+1,
+      name: value.name,
+      description: value.description
+    }
+    videos = [
+      ...videos,
+      newVideo
+    ]
+    return newVideo;
   }
-  videos = [...videos, video];
-  res.send({ data: video, message: `Successfully added ${video.name}`});
-});
+}
 
-app.put('/api/videos/:id', (req, res) => {
-  /**
-   * Look up the video with id,
-   * IF not found 404
-   * 
-   * Validate the input
-   * IF invalid, 400
-   * 
-   * Update videos with input
-   */
-  let video = findVideo(req.params.id);
-  if (!video) return res.status(404).send({ message: 'The video with the given Id does not exist'});
-
-  const errorMsg = validateInput(req.body);
-  if(errorMsg) return res.status(400).send({ message: errorMsg});
-
-  video.name = req.body.name;
-  res.send({ data: video, message: `Successfully edited ${video.name}`});
-});
-
-app.delete('/api/videos/:id', (req, res) => {
-   /**
-   * Look up the video with id,
-   * IF not found 404
-   * 
-   * Delete
-   * 
-   * Return the deleted obj
-   */
-  let video = findVideo(req.params.id);
-  if (!video) return res.status(404).send({ message: 'The video with the given Id does not exist'});
-
+/**
+ * @summary Deletes a video
+ * @param {Object} args
+ * @returns {Object} video
+ */
+const deleteVideo = (args) => {
+  const { id } = args;
+  let video = findVideo(id);
   const index = videos.indexOf(video);
   videos.splice(index, 1);
+  return video
+}
 
-  res.send({ data: video, message: `Successfully deleted ${video.name}`});
-});
+// root resolver
+const root = {
+  video: getVideo,
+  videos: getVideos,
+  updateVideo,
+  createVideo,
+  deleteVideo,
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 const port = process.env.PORT;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Graphql server running on port ${port}...`));
