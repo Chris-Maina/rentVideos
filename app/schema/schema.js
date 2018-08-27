4a148e08d5da1d807a3074a52f34428afbd43336
Chris Maina <chris.maina@andela.com>
2018-08-27 20:33:17 +0300

    feat(CRUD): add queries and mutations
     - add queries to get genres and directors
     - add mutations to add, delete and update a video
     - add mutations to add and delete genres
     - add mutations to add and delete directors

--- app/schema/schema.js
+++ app/schema/schema.js
@@ -89,6 +89,13 @@ const RootQuery = new GraphQLObjectType({
         return response.data;
       }
     },
+    genres: {
+      type: new GraphQLList(GenreType),
+      async resolve() {
+        const response = await axios.get(`http://localhost:3000/genres`);
+        return response.data;
+      }
+    },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLString } },
@@ -96,9 +103,111 @@ const RootQuery = new GraphQLObjectType({
         const response = await axios.get(`http://localhost:3000/directors/${args.id}`);
         return response.data;
       }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      async resolve() {
+        const response = await axios.get(`http://localhost:3000/directors`);
+        return response.data;
+      }
+    }
+  }
+})
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addVideo: {
+      type: VideoType,
+      args: {
+        price: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: GraphQLString },
+        director: { type: GraphQLString }
+      },
+      async resolve (parentValue, args) {
+        const { price, name, description, genre, director } = args;
+        const response = await axios.post(`http://localhost:3000/videos`, {
+          price,
+          name,
+          description,
+          directorId: director,
+          genreId: genre,
+         });
+        return response.data;
+      }
+    },
+    deleteVideo: {
+      type: VideoType,
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLString) }, },
+      async resolve (parentValue, {id}) {
+        const response = await axios.delete(`http://localhost:3000/videos/${id}`);
+        return response.data
+      }
+    },
+    updateVideo: {
+      type: VideoType,
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString},
+        price: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        director: { type: GraphQLString }
+        },
+      async resolve (parentValue, args) {
+        const  {id, name, price, description, genre, director} = args;
+        const response = await axios.patch(`http://localhost:3000/videos/${id}`, {
+          name,
+          description,
+          price,
+          genreId: genre,
+          directorId: director
+        });
+        return response.data
+      }
+    },
+    addGenre: {
+      type: GenreType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString)},
+      },
+      async resolve (parentValue, { name }) {
+        const response = await axios.post(`http://localhost:3000/genres`, { name });
+        return response.data;
+      }
+    },
+    deleteGenre: {
+      type: GenreType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) }},
+      async resolve(parentValue, args) {
+        const response = await axios.delete(`http://localhost:3000/genres/${args.id}`);
+        return response.data
+      }
+    },
+    addDirector: {
+      type: DirectorType,
+      args: {
+        fullName: { type: new GraphQLNonNull(GraphQLString)}
+      },
+      async resolve (parentValue, { fullName }) {
+        const response = await axios.post(`http://localhost:3000/directors`, { fullName });
+        return response.data;
+      }
+    },
+    deleteDirector: {
+      type: DirectorType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) }},
+      async resolve(parentValue, args) {
+        const response = await axios.delete(`http://localhost:3000/directors/${args.id}`);
+        return response.data
+      }
     }
   }
 })
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 })
